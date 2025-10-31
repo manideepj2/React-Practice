7737a1aa5317cac511de3fe2947964a7b4570773
Manideep Jammigumpula <manideepj2@gmail.com>
2025-10-31 16:11:37 -0400

    practiced if with for loop in react

--- src/components/Child.tsx
+++ src/components/Child.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 interface ChildProps {
     message: string;
@@ -5,13 +6,38 @@ interface ChildProps {
 }
 function Child({message,setDataToParent}: Readonly<ChildProps>) {
   const dataToParent="Hello from Child";
+  const [name,setName]=useState<string>("");
+  const [names,setNames]=useState<string[]>([]);
+  const [showNames,setShowNames]=useState<boolean>(false);
+
+  const handleBlur=(value:string):void=>{
+    setNames([...names,value]);
+    setName("");
+  }
+
   return (
     <>
       <div>Child</div>
       <p>message from Parent is : {message}</p>
-      <button onClick={() => setDataToParent(dataToParent)}>Send to parent</button>
+      <button onClick={() => setDataToParent(dataToParent)}>
+        Send to parent
+      </button>
+      <br />
+      <input
+        type="text"
+        placeholder="Enter name"
+        onBlur={(e) => handleBlur(e.target.value)}
+      />
+      <br />
+      {!showNames ? (
+        <button onClick={() => setShowNames(!showNames)}>Display Names</button>
+      ):  <button onClick={() => setShowNames(!showNames)}>Hide Names</button>}
+      <br />
+      <div>
+        {showNames && names.map((name, index) => <p key={index}>{name}</p>)}
+      </div>
     </>
-  )
+  );
 }
 
 export default Child
